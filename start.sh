76acf5e83ad69efb9130cc9d1b2052b9cbc3bc0a
Sneh1999 <snehkoul1999@gmail.com>
2020-07-17 14:25:17 -0400

    Feat: start opencv

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# TODO : check if the dependancy alreay exists
+
+# Get into backend folder
+cd Backend
+# create virtual env
+virtualenv communicate
+
+# activate virtual env
+source ./communicate/bin/activate
+
+# install opencv-python
+pip3 install -r requirements.txt
+
+# Run
+python3 app.py
